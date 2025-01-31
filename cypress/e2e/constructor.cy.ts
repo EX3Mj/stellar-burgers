b445ae70f7e199a5ed2291c07ff382eb236c8a1a
Dmitry Balobanov <dm.balobanov@yandex.ru>
2025-01-31 14:39:02 +0300

    pr12 fix after first review

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -29,49 +29,52 @@ describe('constructor test', () => {
     beforeEach(() => {
       cy.get('[data-cy="modal"]').should('not.exist');
       cy.get('[data-cy="main"]').first().click();
+      cy.get('[data-cy="modal"]').as('modal').should('be.visible');
     });
 
     it('modal is opened with ingredient detail', () => {
-      cy.get('[data-cy="modal"]').should('be.visible');
       cy.contains('Детали ингредиента').should('exist');
     });
 
     it('modal closed by button', () => {
-      cy.get('[data-cy="modal"]').should('be.visible');
+      cy.get('@modal').should('be.visible');
       cy.get('[data-cy="modal-close"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.get('@modal').should('not.exist');
     });
     it('modal closed by overlay click', () => {
-      cy.get('[data-cy="modal"]').should('be.visible');
+      cy.get('@modal').should('be.visible');
       cy.get('[data-cy="modal-overlay"]').click({ force: true });
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.get('@modal').should('not.exist');
     });
   });
 
   describe('test order', () => {
-
     beforeEach(() => {
       cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
       cy.setCookie('accessToken', 'testaccesstoken');
       localStorage.setItem('refreshToken', 'testrefreshtoken');
       cy.visit('/');
     });
-  
+
     afterEach(() => {
       cy.setCookie('accessToken', '');
       localStorage.setItem('refreshToken', '');
     });
 
     it('test order create', () => {
-      cy.intercept('POST', 'api/orders', { fixture: 'order.json' });
+      cy.intercept('POST', 'api/orders', { fixture: 'order.json' }).as('createOrder');
       cy.get('[data-cy="bun"]').contains('Добавить').click();
       cy.get('[data-cy="main"]').contains('Добавить').click();
       cy.get('[data-cy="sauce"]').contains('Добавить').click();
       cy.contains('Оформить заказ').click();
-      cy.get('[data-cy="modal"]').should('be.visible');
-      cy.get('[data-cy="modal"]').contains('67224').should('exist');
+      cy.wait('@createOrder').then((interception) => {
+        const responseBody = interception.response.body;
+        expect(responseBody.order.number).to.equal(67224);
+      });
+      cy.get('[data-cy="modal"]').should('be.visible').as('modal');
+      cy.get('@modal').contains('67224').should('exist');
       cy.get('[data-cy="modal-close"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.get('@modal').should('not.exist');
       cy.get('div').contains('Выберите булки').should('exist');
       cy.get('div').contains('Выберите начинку').should('exist');
     });
